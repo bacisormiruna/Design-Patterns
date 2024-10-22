28ebacb4885c9a9c2fd0aeb6d5de2ed27e906a1e
bacisormiruna <mirunabacisor06@gmail.com>
2024-10-22 20:53:32 +0300

    Added classes for Proxy

---
+++ src/Structural/Proxy/some_cool_media_library/ThirdPartyYoutubeLib.java
@@ -0,0 +1,9 @@
+package Structural.Proxy.some_cool_media_library;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYoutubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}

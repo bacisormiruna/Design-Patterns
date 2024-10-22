bc09e97e1345ace05e64b5a28e6f3c2ed75865b0
bacisormiruna <mirunabacisor06@gmail.com>
2024-10-22 21:26:22 +0300

    Added classes for Strategy

--- src/Behavioral/Strategy/strategies/PayStrategy.java
+++ src/Behavioral/Strategy/strategies/PayStrategy.java
@@ -1,2 +1,6 @@
-package Behavioral.Strategy.strategies;public interface PayStrategy {
+package Behavioral.Strategy.strategies;
+
+public interface PayStrategy {
+    boolean pay(int paymentAmount);
+    void collectPaymentDetails();
 }

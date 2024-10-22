8dfc981b6d3b0a276d6848e05cbcb0aa2293e6a2
bacisormiruna <mirunabacisor06@gmail.com>
2024-10-22 20:02:59 +0300

    Added classes for Adapter

---
+++ src/Structural/Adapter/round/RoundHole.java
@@ -0,0 +1,18 @@
+package Structural.Adapter.round;
+
+public class RoundHole {
+    public double radius;
+    public RoundHole(double radius){
+        this.radius=radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg){
+        boolean result;
+        result=(this.getRadius()>=peg.getRadius());
+        return result;
+    }
+}
